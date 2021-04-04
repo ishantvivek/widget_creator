35a1be4538971c48b910f03b74aee77e029e3dd4
Ishant Vivek <ishantvivek1311@gmail.com>
2021-04-04 15:33:02 +0530

    Fetch dataSet from src
    * We fetch dataSet using Axios from https://api.covid19india.org/state_district_wise.json and extract States name
    * Select State now gets values from that json data only

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,57 +1,77 @@
-import React from "react"
+import React from 'react'
+import fetchApi from '../utils/fetchApi'
 
 
-function MainContent(){
-    return(
-        <form>
-            <h2> Enter Widget Details </h2>
+export default class MainContent extends React.Component {
+    constructor(props){
+        super(props);
 
-            <div className="form">
-                <label> Select State: </label>
-                <select>
-                    <option value="dummy1">Dummy1</option>
-                    <option value="dummy2">Dummy2</option>
-                </select>
+        this.state = {
+            states : []
+        }
+    }
+
+    async componentDidMount() {
+        let list = [] , dataset = await fetchApi.get()
+        dataset = dataset.data
+        for(const stateName in dataset){
+            list.push({name: stateName})
+        }
+        console.log(dataset)
+        this.setState({states: list})
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <h2> Enter Widget Details </h2>
+                    <div className="form">
+                        <label> Select State: </label>
+                        <select>
+                        {this.state.states.map((e, key) => {
+                            return <option key={key} value={e.statecode}> {e.name}</option>
+                        })}
+                        </select>
+                    </div>
+
+                    <div className="form">
+                    <label>Header Text: <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Header Background: <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Header Text Color: <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Footer Text: <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Footer Background: <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Footer Text Color: <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Width(in px): <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <label>Height(in px): <input type="text" name="name" /> </label>
+                    </div>
+
+                    <div className="form">
+                    <button type="button">Get your Widget</button>
+                    </div>
+                </form>
             </div>
-
-            <div className="form">
-            <label>Header Text: <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Header Background: <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Header Text Color: <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Footer Text: <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Footer Background: <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Footer Text Color: <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Width(in px): <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <label>Height(in px): <input type="text" name="name" /> </label>
-            </div>
-
-            <div className="form">
-            <input type="submit" value="Get your widget" name="Get your widget" />
-            </div>
-
-        </form>
-    )
+        )
+    }
 }
-
-export default MainContent
